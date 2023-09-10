ccc00707f9e6f15b36be549a0d68ae7aa5cf6269
Jorrit <vangilsjorrit@gmail.com>
2023-09-10 11:34:14 -0500

    project with working store

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>todo form</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    todos: [
+        {
+            title: 'todo item a',
+            completed: false
+        },
+        {
+            title: 'todo item b',
+            completed: false
+        }
+    ]
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+  }
+})
